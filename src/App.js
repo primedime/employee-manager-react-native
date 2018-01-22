b7b414519178d415769eeaccf4361126c81edc1f
Roy Jossfolk <rjossfolk@gmail.com>
2018-01-22 16:17:17 -0500

    - add common components + create LoginForm.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import firebase from 'firebase';
 import reducers from './reducers';
+import LoginForm from './components/LoginForm';
 
 
 export default class App extends Component {
@@ -28,11 +29,7 @@ export default class App extends Component {
   render() {
     return (
       <Provider store={createStore(reducers)}>
-        <View>
-          <Text>
-          Hello!
-          </Text>
-        </View>
+        <LoginForm />
       </Provider>
     );
   }
---
+++ src/components/LoginForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { Card } from './common';
+
+export default class LoginForm extends Component {
+    render() {
+        return (
+            <Card>
+            </Card>
+        );
+    }
+}

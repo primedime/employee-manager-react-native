ae2fa80ea0960b890e9b6ffb40fc78ec7f8a3bee
Roy Jossfolk <rjossfolk@gmail.com>
2018-01-22 16:33:52 -0500

    - finish LoginForm UI

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
-import { Card } from './common';
+import { Card, CardSection, Input, Button } from './common';
 
 export default class LoginForm extends Component {
     render() {
         return (
             <Card>
+                <CardSection>
+                    <Input
+                        label="Email"
+                        placeholder="email@gmail.com" 
+                    />
+                </CardSection>
+                
+                <CardSection>
+                <Input
+                    secureTextEntry
+                    label="Password"
+                    placeholder="password" 
+                />
+                </CardSection>
+
+                <CardSection>
+                    <Button>
+                        Login
+                    </Button>
+                </CardSection>
             </Card>
         );
     }

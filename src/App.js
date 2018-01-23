63737ee2a529b6dfd2c20dab2bfeb70bd5a22da9
Roy Jossfolk <rjossfolk@gmail.com>
2018-01-22 21:43:19 -0500

    - connect async action creator for loginUser

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React, { Component } from 'react';
-import {
-  Text,
-  View
-} from 'react-native';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import firebase from 'firebase';
+import ReduxThunk from 'redux-thunk';
 import reducers from './reducers';
 import LoginForm from './components/LoginForm';
 
@@ -14,19 +11,21 @@ export default class App extends Component {
 
   componentWillMount() {
     const config = {
-      apiKey: "AIzaSyC9EYPuBOJ1ng8Z9Pk7_l8hL_em8Z1U23c",
-      authDomain: "manager-b5d44.firebaseapp.com",
-      databaseURL: "https://manager-b5d44.firebaseio.com",
-      projectId: "manager-b5d44",
-      storageBucket: "manager-b5d44.appspot.com",
-      messagingSenderId: "275753443447"
+      apiKey: 'AIzaSyC9EYPuBOJ1ng8Z9Pk7_l8hL_em8Z1U23c',
+      authDomain: 'manager-b5d44.firebaseapp.com',
+      databaseURL: 'https://manager-b5d44.firebaseio.com',
+      projectId: 'manager-b5d44',
+      storageBucket: 'manager-b5d44.appspot.com',
+      messagingSenderId: '275753443447'
     };
     firebase.initializeApp(config);
   }
 
   render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+
     return (
-      <Provider store={createStore(reducers)}>
+      <Provider store={store}>
         <LoginForm />
       </Provider>
     );
--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -8,6 +8,7 @@ const INITIAL_STATE = {
 };
 
 export default (state = INITIAL_STATE, action) => {
+    console.log(action);
     switch (action.type) {
         case EMAIL_CHANGED:
             return { ...state, email: action.payload };

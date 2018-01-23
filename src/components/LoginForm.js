2fd368156d5b6afbe9b42ca1034f0cb0be2febe9
Roy Jossfolk <rjossfolk@gmail.com>
2018-01-22 22:39:04 -0500

    - add helper function loginUserFail + createUserWithEmailAndPassword

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { View, Text } from 'react-native';
 import { connect } from 'react-redux';
 import { emailChanged, passwordChanged, loginUser } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
@@ -17,6 +18,18 @@ class LoginForm extends Component {
         const { email, password } = this.props;
 
         this.props.loginUser({ email, password });
+    }
+
+    renderError() {
+        if (this.props.error) {
+            return (
+                <View style={{ backgroundColor: 'white' }}>
+                    <Text style={styles.errorTextStyle}>
+                    {this.props.error}
+                    </Text>
+                </View>
+            );
+        }
     }
 
     render() {
@@ -41,6 +54,8 @@ class LoginForm extends Component {
                 />
                 </CardSection>
 
+                {this.renderError()}
+
                 <CardSection>
                     <Button onPress={this.onButtonPress.bind(this)}>
                         Login
@@ -51,10 +66,19 @@ class LoginForm extends Component {
     }
 }
 
+const styles = {
+    errorTextStyle: {
+        fontSize: 20,
+        alignSelf: 'center',
+        color: 'red'
+    }
+};
+
 const mapStateToProps = state => {
     return {
         email: state.auth.email,
-        password: state.auth.password
+        password: state.auth.password,
+        error: state.auth.error
     };
 };
 
--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,12 +1,14 @@
 import { EMAIL_CHANGED, 
         PASSWORD_CHANGED,
-        LOGIN_USER_SUCCESS
+        LOGIN_USER_SUCCESS,
+        LOGIN_USER_FAIL
 } from '../actions/types';
 
 const INITIAL_STATE = { 
     email: '',
     password: '',
-    user: null 
+    user: null,
+    error: '' 
 };
 
 export default (state = INITIAL_STATE, action) => {
@@ -18,6 +20,8 @@ export default (state = INITIAL_STATE, action) => {
             return { ...state, password: action.payload };
         case LOGIN_USER_SUCCESS:
             return { ...state, user: action.payload };
+        case LOGIN_USER_FAIL:
+             return { ...state, error: 'Athentication Failed.', password: '' };
         default:
             return state;
     }

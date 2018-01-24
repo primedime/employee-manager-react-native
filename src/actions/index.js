a8adf15993b5a9acbf649cae812c263c6ac46747
Roy Jossfolk <rjossfolk@gmail.com>
2018-01-23 23:10:33 -0500

    - display loading spoinner on login attempt

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,8 @@ import firebase from 'firebase';
 import { EMAIL_CHANGED, 
         PASSWORD_CHANGED,
         LOGIN_USER_SUCCESS,
-        LOGIN_USER_FAIL 
+        LOGIN_USER_FAIL,
+        LOGIN_USER
 } from './types';
 
 export const emailChanged = (text) => {
@@ -21,6 +22,8 @@ export const passwordChanged = (text) => {
 
 export const loginUser = ({ email, password }) => {
     return (dispatch) => {
+        dispatch({ type: LOGIN_USER });
+
         firebase.auth().signInWithEmailAndPassword(email, password)
         .then(user => loginUserSuccess(dispatch, user))
         .catch((error) => {
